f022992db78d06accc595e2824021cad768fa4cd
Chris Resnick <chris.resnick@gmail.com>
2020-11-13 13:51:05 -0500

    phase 2

---
+++ src/AppWithContext.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import TodoContext from "./contexts/TodoContext";
+import App from "./App";
+
+class AppWithContext extends React.Component{
+    constructor() {
+        super();
+        const storedTasks = JSON.parse(localStorage.getItem("tasks"));
+        this.state = {
+            createTask: this.createTask,
+            deleteTask: this.deleteTask,
+            tasks: storedTasks || {}
+        }
+    }
+
+    createTask = (task) => {
+        const nextTaskId = (new Date()).getTime();
+        const newTask =   {
+            [nextTaskId]:
+            {
+                id: nextTaskId,
+                message: task,
+            },
+        }
+        this.setState((state, props) => ({
+            tasks: {...state.tasks, ...newTask}
+        }),
+        () => this.updateLocalStorageTasks())
+    };
+
+    deleteTask = (id) => {
+        this.setState((state, props) => {
+            const tasksWithDeletion = { ...state.tasks };
+            delete tasksWithDeletion[id];
+            return {
+              tasks: tasksWithDeletion,
+            };
+          },
+            () => this.updateLocalStorageTasks());;
+    };
+
+    updateLocalStorageTasks = () => {
+        console.log(this.state.tasks);
+        const jsonTasks = JSON.stringify(this.state.tasks);
+        localStorage.setItem('tasks', jsonTasks);
+    }
+
+    render() {
+        return (
+            null
+          // TODO: Use a Provider component to wrap the App component
+          // TODO: Use the AppWithContext state as the Provider component's `value`
+        );
+    }
+}
+
+export default AppWithContext;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import AppWithContext from "./AppWithContext"
+import TodoContext from "./contexts/TodoContext"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TodoContext.Provider value={AppWithContext.state}>
+      <App />
+    </TodoContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
